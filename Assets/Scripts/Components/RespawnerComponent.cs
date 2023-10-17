2aebb14578dac7b10b1111acc6bca672610365d3
Pavel Kyslytsia <ssoteyll@gmail.com>
2023-10-18 02:44:56 +0300

    Add spawnpoints

---
+++ Assets/Scripts/Components/RespawnerComponent.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using TMPro;
+using UnityEngine;
+
+namespace Components
+{
+    public class RespawnerComponent : SerializedMonoBehaviour
+    {
+        [OdinSerialize]
+        public CharacterComponent Character { get; set; }
+        
+        [OdinSerialize]
+        public TextMeshProUGUI Text { get; set; }
+        
+        [OdinSerialize]
+        public SpawnPointComponent SpawnPoint { get; set; }
+
+        private Rigidbody _characterRigidbody;
+        
+
+        private void Start()
+        {
+            Character.Entity.Health.Died += CharacterOnDied;
+            _characterRigidbody = Character.GetComponent<Rigidbody>();
+        }
+
+        private void CharacterOnDied(object sender, EventArgs e)
+        {
+            StartCoroutine(WaitBeforeResurrect());
+        }
+
+        private IEnumerator WaitBeforeResurrect()
+        {
+            for (int i = 3; i > 0; i--)
+            {
+                Text.text = i.ToString();
+                yield return new WaitForSeconds(1);
+            }
+
+            Text.text = "";
+            Character.Entity.Health.Resurrect();
+            Character.transform.position = SpawnPoint.Point;
+            _characterRigidbody.velocity = new Vector3();
+        }
+
+        private void OnEnable()
+        {
+            Character.Entity.Health.Died += CharacterOnDied;
+        }
+
+        private void OnDisable()
+        {
+            Character.Entity.Health.Died -= CharacterOnDied;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent<SpawnPointComponent>(out var spawnPoint))
+            {
+                SpawnPoint = spawnPoint;
+            }
+        }
+    }
+}
